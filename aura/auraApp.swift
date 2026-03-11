c2f3cbd6fb691fcd93b1d46dc983b151a399fae7
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2026-03-10 17:18:07 -0700

    Initial Commit

---
+++ aura/auraApp.swift
@@ -0,0 +1,32 @@
+//
+//  auraApp.swift
+//  aura
+//
+//  Created by Sukhman Singh on 3/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct auraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
